2d14cdddef64915fdb33bff15f612feeaa9346ab
Rijvi Islam <rijviislam54@gmail.com>
2024-02-18 19:21:37 +0600

    feat: added new functionality on select your seat section

--- myscript.js
+++ myscript.js
@@ -1,33 +1,38 @@
 console.log("connected!");
-
+let seatArr = [];
 let availableSeats = document.getElementById("availableSeat").innerText;
 let totalavailableSeats = parseInt(availableSeats);
-
-// console.log(totalavailableSeats)
+let countSeats = document.getElementById("countSeat").innerText;
+let totalCountedSeats = parseInt(countSeats);
+const input = document.getElementById("inp");
+const applyBtn = document.getElementById("apply");
 
 function setSeatBgColor(seatId) {
   const seat = document.getElementById(seatId);
   seat.classList.add("seat_bg");
-  console.log(seatId);
 }
 
-let countSeats = document.getElementById("countSeat").innerText;
-let totalCountedSeats = parseInt(countSeats);
-// console.log(totalCountedSeats);
+function setTextColor(eId) {
+  const settext = document.getElementById(eId);
+  settext.classList.remove("kdbTextColor");
+  settext.classList.add("setTextColor");
+}
 
 const seatPlan = document.getElementsByClassName("kbd");
 
-// console.log(seatPlan);
 for (const eachSeat of seatPlan) {
   eachSeat.addEventListener("click", () => {
-    // console.log(eachSeat.innerText);
     const getSeatId = eachSeat.innerText;
     setSeatBgColor(getSeatId);
+    setTextColor(getSeatId);
+    console.log(getSeatId);
     document.getElementById("availableSeat").innerText =
       totalavailableSeats -= 1;
     document.getElementById("countSeat").innerText = totalCountedSeats += 1;
 
     const seatTitile = document.getElementById("seatTitle");
+
+    eachSeat.setAttribute("disabled", "true");
 
     const div = document.createElement("div");
 
@@ -45,10 +50,52 @@ for (const eachSeat of seatPlan) {
     div.appendChild(p3);
 
     let ticketTotal = document.getElementById("totalPrice");
-let ticketPrice = ticketTotal.innerText;
+    let ticketPrice = ticketTotal.innerText;
     let totalTicketFare = parseInt(ticketPrice);
     totalTicketFare += 550;
     ticketTotal.innerText = totalTicketFare;
-    console.log(totalTicketFare);
   });
 }
+
+applyBtn.addEventListener("click", () => {
+  const inputValue = input.value;
+  const totalTicketPrice = document.getElementById("totalPrice").innerText;
+  const totalTicketFare = parseInt(totalTicketPrice);
+  const grandTotalPrice = document.getElementById("grandTotal");
+  const inputApply = document.getElementById("inpApply");
+  console.log(totalTicketFare);
+
+  //   NEW 15 DISCOUNT CALCULATION //
+  if (inputValue === "New15") {
+    if (totalTicketFare >= 550) {
+      const discountPrice = totalTicketFare * 0.15;
+      const totalGrandPrice = totalTicketFare - discountPrice;
+      grandTotalPrice.innerText = totalGrandPrice;
+      inputApply.classList.add("hidden");
+    } else {
+      return alert(
+        "Please purchease your ticket then you will get a discount!"
+      );
+    }
+    console.log(totalTicketFare);
+  } else if (inputValue === "Couple 20") {
+    if (totalTicketFare >= 550) {
+      const discountPrice = totalTicketFare * 0.15;
+      const totalGrandPrice = totalTicketFare - discountPrice;
+      grandTotalPrice.innerText = totalGrandPrice;
+      inputApply.classList.add("hidden");
+    } else {
+      return alert(
+        "Please purchease your ticket then you will get a discount!"
+      );
+    }
+
+    //   COUPLE 20 DISCOUNT CALCULATION //
+
+    console.log(totalTicketFare);
+  } else {
+    return alert("invalid coupon code!");
+  }
+
+  //   console.log(inputValue);
+});
